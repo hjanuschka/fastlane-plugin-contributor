c30f92f407b2d6405875ccd06c1ab53bad461353
Helmut Januschka <h.januschka@krone.at>
2016-11-21 10:17:30 +0100

    u

--- lib/fastlane/plugin/contributor/actions/split_pr_action.rb
+++ lib/fastlane/plugin/contributor/actions/split_pr_action.rb
@@ -5,10 +5,33 @@ module Fastlane
         # ensure git status is clean
         other_action.ensure_git_status_clean
         UI.important("Resetting to #{params[:base_branch]}")
-        FastlaneCore::CommandExecutor.execute(command: "git reset --soft #{params[:base_branch]}",
+        FastlaneCore::CommandExecutor.execute(command: "git reset #{params[:base_branch]}",
                                           print_all: true,
                                       print_command: true
                                               )
+
+        seperated_prs = []
+        `git status -u -s --porcelain`.split("\n").each do | l |
+          entry = l.split(" ")
+          file_name = entry[1]
+          module_info = file_name.split("/")
+          mod = module_info.first
+          seperated_prs << mod
+        end
+        
+        current_branch=other_action.git_branch
+        
+        seperated_prs.each do | m |
+          FastlaneCore::CommandExecutor.execute(command: "git checkout -B #{current_branch}_#{m}",
+                                            print_all: true,
+                                        print_command: true
+                                                )
+          other_action.git_add(path: "#{File.expand_path(m)}/")
+          other_action.push_to_git_remote(force: true)
+          FastlaneCore::CommandExecutor.execute(command: "git checkout #{current_branch}",
+                                              print_all: true,
+                                          print_command: true)
+        end
         
       end
 
